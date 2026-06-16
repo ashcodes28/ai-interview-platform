09b1bdb8327f26c12101c5fd04ae467f67c07228
ashcodes28 <aishwaryasreenivasan28@gmail.com>
2026-06-16 21:18:28 +0530

    Added interview router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.routes import interview
 
 app = FastAPI()
 
+app.include_router(interview.router)
+
 @app.get("/")
 def root():
-    return {"message": "AI Interview Platform Backend Running"}
+    return {
+        "message": "AI Interview Platform Backend Running"
+    }
